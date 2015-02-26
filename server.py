3ab75167883874a0e44757bb504535921b245c48
Mikael Frykholm <mikael@frykholm.com>
2015-02-26 14:57:52 +0100

    Save Content-Type as xattr for much performance

--- server.py
+++ server.py
@@ -8,18 +8,17 @@ import tornado.web
 from tornado.options import define, options
 import xattr
 
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self, arg, head=False):
         if not arg:
             self.redirect("/post.html")
         path = os.path.join('files',arg)
         try:
-            with open(path,"rb") as f: 
-                self.set_header("Expires", datetime.datetime.utcnow() + datetime.timedelta(1000000)) 
-                mimetype = Popen(["file","-b","--mime-type", path], stdout=PIPE).communicate()[0].decode('utf8').strip()
-                self.set_header("Content-Type", mimetype)
+            with open(path,"rb") as f:
                 attrs = xattr.xattr(f)
+                self.set_header("Expires", datetime.datetime.utcnow() + datetime.timedelta(1000000)) 
+                if 'user.Content-Type' in xattrs:
+                    self.set_header("Content-Type", xattrs['user.Content-Type'].decode('utf-8'))
                 try:
                     orig_filename = attrs.get('user.filename').decode('utf-8')
                     self.set_header('Content-Disposition',' inline; filename="{}"'.format(orig_filename))
@@ -42,17 +41,22 @@ class MainHandler(tornado.web.RequestHandler):
             self.finish()
             return
         filename = base64.urlsafe_b64encode(hashlib.sha256(file_body).digest()).decode('utf-8') 
-        with open(os.path.join('files',filename),"wb") as f:
+        with open(os.path.join('files', filename), "wb") as f:
             f.write(file_body)
+            attrs = xattr.xattr(f)
+            mimetype = Popen(["file", "-b","--mime-type", f.name], stdout=PIPE).communicate()[0].decode('utf8').strip()
+            attrs['user.Content-Type'] = mimetype.encode('utf-8')
         self.write('<html><body><a href="http://h45h.com/{}"></body></html>{}'.format(filename,filename))
+
     def put(self, arg):
         filename = base64.urlsafe_b64encode(hashlib.sha256(self.request.body).digest()).decode('utf-8')
         with open(os.path.join('files',filename),"wb") as f:
             f.write(self.request.body)
             attrs = xattr.xattr(f)
-            attrs.set(b'user.filename',arg.encode('utf-8'))
+            mimetype = Popen(["file", "-b","--mime-type", f.name], stdout=PIPE).communicate()[0].decode('utf8').strip()
+            attrs['user.Content-Type'] = mimetype.encode('utf-8')
+            attrs['user.filename'] =  arg.encode('utf-8')
         self.write('http://h45h.com/{}\n'.format(filename))       
-
 
 application = tornado.web.Application([
     (r"/(.*\.html)", tornado.web.StaticFileHandler,     dict(path=os.path.join(os.path.dirname(__file__)))),
